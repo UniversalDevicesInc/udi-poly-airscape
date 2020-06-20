85ec4d79351e430e1c8839336d5f0c149d69b4a8
JimBo CA <jimboca3@gmail.com>
2020-06-19 19:31:47 -0700

    Trying to get DON working with Google Home

--- nodes/Airscape2.py
+++ nodes/Airscape2.py
@@ -32,6 +32,7 @@ class Airscape2(polyinterface.Node):
         self.do_poll = True
 
     def shortPoll(self):
+        pass
         #if 'doorinprocess' in self.status and int(self.status['doorinprocess']) == 1:
         #self.l_debug('shortPoll', '...')
         #if not self.watching_door:
