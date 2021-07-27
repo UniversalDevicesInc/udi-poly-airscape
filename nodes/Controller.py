76ac34578b815f445f4be4620960a592b8a5a7ad
JimBoCA <jimboca3@gmail.com>
2021-07-26 20:37:01 -0700

    Working on PG3 converstion

--- nodes/Controller.py
+++ nodes/Controller.py
@@ -26,7 +26,7 @@ class Controller(Node):
         poly.addNode(self)
 
     def handler_start(self):
-        LOGGER.info('help')
+        LOGGER.info(f'help {self}')
         serverdata = self.poly.get_server_data()
         LOGGER.info(f'Started Airscape NodeServer {serverdata['version']}')
         self.Notices.clear()
