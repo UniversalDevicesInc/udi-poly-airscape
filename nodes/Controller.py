61ab3b7a5021deffccaa43904779390735a5a1c0
JimBoCA <jimboca3@gmail.com>
2021-07-26 20:39:02 -0700

    Working on PG3 converstion

--- nodes/Controller.py
+++ nodes/Controller.py
@@ -27,7 +27,7 @@ class Controller(Node):
 
     def handler_start(self):
         serverdata = self.poly.get_server_data()
-        LOGGER.info(f"Started Airscape NodeServer {serverdata['version']}"")
+        LOGGER.info(f"Started Airscape NodeServer {serverdata['version']}")
         self.Notices.clear()
         self.heartbeat()
         self.check_profile()
